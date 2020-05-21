7b4fa043e382ee9be417ffee63349bf1d47618d7
c3nsored <u.gabrielr@gmail.com>
2020-05-21 16:58:43 +0200

    fixed attributeError casued by invalid file path

--- image-processing/main.py
+++ image-processing/main.py
@@ -11,13 +11,13 @@ Templating matching using opencv
 def main(image):
     # create an image copy
     img_copy = image.copy()
-    template = cv2.imread('images/template.jpg', 0)
+    template = cv2.imread('image-processing/images/template.jpg', 0)
     w, h = template.shape[::-1]
 
     # Method for template matchin
     methods = 'cv2.TM_CCOEFF_NORMED'
-    # _methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
-    #         'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+
+    _methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR','cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
     # method = eval(method)
 
     #applying template matching 
@@ -26,27 +26,27 @@ def main(image):
     res = cv2.matchTemplate(img_copy,template, method)
     min_val, max_val, min_loc, max_loc =  cv2.minMaxLoc(res)
 
-    # if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-    #     top_left = min_loc
-    # else:
-    #     top_left = max_loc
-    
-    top_left = min_loc
+    # TODO: minMAXLoc needs a review
+    top_left = min_loc 
     bottom_right = (top_left[0] + w, top_left[1] + h)
 
     cv2.rectangle(img_copy, top_left, bottom_right, (0,255,0), 20)
 
+
+    #Plotting --------------------------------------------
     plt.subplot(121), plt.imshow(res, cmap = 'gray')
     plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+    
     plt.subplot(122),plt.imshow(img_copy, cmap = 'gray')
     plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-    plt.suptitle(method)
-
+    
+    plt.suptitle(methods)
     plt.show()
 
 if __name__ == "__main__":
-    mypath = 'images/samples/image-00000.jpg'
-    cv_image = cv2.imread(mypath, 0)
+    image_path = "image-processing/images/image-00000.jpg"
+    cv_image = cv2.imread(image_path, 0)
+    print(cv_image)
     main(cv_image)
     
     # ----------------------------------------------------------
