1dc32042887b0130bf48fd0d469e9d9726148dbc
c3nsored <ur.gabriel@outlook.com>
2020-05-25 01:13:57 +0200

    image brightness

--- image-processing/exp1.py
+++ image-processing/exp1.py
@@ -14,23 +14,18 @@ def contains_template(template_image, visualize=False, image_path="images"):
     for imagePath in glob.glob(image_path + "/*.jpg"):
         img = cv.imread(imagePath)
         
-        scale_percent = 10 # percent of original size
-        width = int(img.shape[1] * scale_percent / 100)
-        height = int(img.shape[0] * scale_percent / 100)
-        dim = (width, height)
-        """ resize image"""
-        image = cv.resize(img, dim, interpolation = cv.INTER_AREA)
-        _gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        _gray = cv.cvtColor(image_resize(img, scale_percent=30), cv.COLOR_BGR2GRAY)
     
-        for scale in np.linspace(0.2, 1.0, 50)[::-1]:
+        for scale in np.linspace(0.2, 1.0, 30)[::-1]:
 
             resized = imutils.resize(_gray, width=int(_gray.shape[1] * scale))
             r = _gray.shape[1]/float(resized.shape[1])
 
             if resized.shape[0] < tH or resized.shape[1] < tW:
                 break 
-
-            edged = cv.Canny(resized, 50, 200)
+            
+            _image = 0
+            edged = cv.Canny(resized, 50, 100)
             result = cv.matchTemplate(edged, template_image, cv.TM_CCOEFF)
             # experiment
             # w, h = template.shape[::-1] 
@@ -64,17 +59,69 @@ def contains_template(template_image, visualize=False, image_path="images"):
         cv.imshow("Image", image)
         cv.waitKey(0)
 
+def image_resize(img, scale_percent=50):
+        width = int(img.shape[1] * scale_percent / 100)
+        height = int(img.shape[0] * scale_percent / 100)
+        dim = (width, height)
+        """ resize image"""
+        image = cv.resize(img, dim, interpolation = cv.INTER_AREA)
+        
+        return image
 
+        
 def canny_template(image):
     #converts image to gray 
-    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    img = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+    scale_percent = 250 # percent of original size
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    """ resize image"""
+    image = cv.resize(img, dim, interpolation = cv.INTER_AREA)
     # applies canny edge detection 
-    template = cv.Canny(gray, 50, 200)
+    template = cv.Canny(image, 50, 200)
     
     return template
 
+def filtered(gray_image):
+    blur = cv.GaussianBlur(gray_image, (11, 11), 0)
+    thresh = cv.threshold(blur, 200, 255, cv.THRESH_BINARY)[1]
+    thresh = cv.erode(thresh, None, iterations=2)
+    thresh = cv.dilate(thresh, None, iterations=4)
+
+def test_funt(image):
+
+    # image = image_resize(img)
+    new_image = np.zeros(image.shape, image.dtype)
+    
+    alpha = 1.0 # Simple contrast control
+    beta = 50    # Simple brightness control
+    # Initialize values
+    print(' Basic Linear Transforms ')
+    print('-------------------------')
+    try:
+        alpha = float(0.0) # float: 1.0-3.0
+        beta = int(100) # 1-100
+    except ValueError:
+        print('Error, not a number')
+    # Do the operation new_image(i,j) = alpha*image(i,j) + beta
+    # Instead of these 'for' loops we could have used simply:
+    # new_image = cv.convertScaleAbs(image, alpha=alpha, beta=beta)
+    # but we wanted to show you how to access the pixels :)
+    for y in range(image.shape[0]):
+        for x in range(image.shape[1]):
+            for c in range(image.shape[2]):
+                new_image[y,x,c] = np.clip(alpha*image[y,x,c] + beta, 0, 255)
+    cv.imshow('Original Image', image)
+    cv.imshow('New Image', new_image)
+    # Wait until user press some key
+    cv.waitKey()
 
 if __name__ == "__main__":
-    template = canny_template(cv.imread("11.jpg"))
+    template = canny_template(cv.imread("13.jpg"))
     print(template.shape[:2])
-    contains_template(template, visualize=True)
+    image = cv.imread("images/2.jpg")
+    resized = image_resize(image)
+    test_funt(resized)
+    # contains_template(template, visualize=True)
