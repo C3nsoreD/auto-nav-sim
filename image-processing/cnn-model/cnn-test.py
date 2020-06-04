bed3cf7ff7ea1eb86e0499b30b385f4b51cf4271
Gabriel C3n <ur.gabriel@outlook.com>
2020-06-04 13:33:25 +0200

    image classifier

--- image-processing/cnn-model/cnn-test.py
+++ image-processing/cnn-model/cnn-test.py
@@ -1,71 +1,84 @@
-import tensorflow as tf 
+import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras import layers
 import pickle
-from keras.models import model_from_json
-from keras.models import load_model
-import matplotlib.pyplot as plt
+import keras
 
-# Opening the files about data
+import numpy as np
+from keras_preprocessing.image import load_img
+from keras_preprocessing.image import img_to_array
+from keras_preprocessing.image import ImageDataGenerator
+
+
+# Loading the datasets
 X = pickle.load(open("X.pickle", "rb"))
 y = pickle.load(open("y.pickle", "rb"))
+y = np.asarray(y)
 
-# normalizing data (a pixel goes from 0 to 255)
-X = X/255.0
+image_size = (100, 100)
+batch_size = 32
 
-# Building the model
-model = Sequential()
-# 3 convolutional layers
-model.add(Conv2D(32, (3, 3), input_shape = X.shape[1:]))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2,2)))
 
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2,2)))
+def make_model(input_shape, num_classes):
+    inputs = keras.Input(shape=input_shape)
+    # Image augmentation block
+    x = layers.Conv2D(32, 3, strides=2, padding="same")(X)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
 
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Dropout(0.25))
+    x = layers.Conv2D(64, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
 
-# 2 hidden layers
-model.add(Flatten())
-model.add(Dense(128))
-model.add(Activation("relu"))
+    previous_block_activation = x
+    for size in [128, 256, 512, 728]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
 
-model.add(Dense(128))
-model.add(Activation("relu"))
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
 
-# The output layer with 13 neurons, for 13 classes
-model.add(Dense(13))
-model.add(Activation("softmax"))
+        x = layers.MaxPool2D(3, strides=2, padding="same")(x)
 
-# Compiling the model using some basic parameters
-model.compile(loss="sparse_categorical_crossentropy",
-				optimizer="adam",
-				metrics=["accuracy"])
+        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])
+        previous_block_activation = x
 
-# Training the model, with 40 iterations
-# validation_split corresponds to the percentage of images used for the validation phase compared to all the images
-history = model.fit(X, y, batch_size=32, epochs=40, validation_split=0.1)
+    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
 
-# Saving the model
-model_json = model.to_json()
-with open("model.json", "w") as json_file :
-	json_file.write(model_json)
+    x = layers.GlobalAveragePooling2D()(x)
+    if num_classes == 2:
+        activation = "sigmoid"
+        units = 2
+    else:
+        activation = "softmax"
+        units = num_classes
+    x = layers.Dropout(0.5)(x)
+    outputs = layers.Dense(units, activation=activation)(x)
+    return keras.Model(inputs, outputs)
 
-model.save_weights("model.h5")
-print("Saved model to disk")
 
-model.save('CNN.model')
+model = make_model(input_shape=image_size + (3, ), num_classes=2)
+keras.utils.plot_model(model, show_shapes=True)
 
-# Printing a graph showing the accuracy changes during the training phase
-print(history.history.keys())
-plt.figure(1)
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'validation'], loc='upper left')
+epochs = 10
+
+callbacks = [
+    keras.callbacks.ModelCheckpoint("save_at_{epoch}.h5"),
+]
+model.compile(
+    optimizer=keras.optimizers.Adam(1e-3),
+    loss="binary_crossentropy",
+    metrics=["accuracy"],
+)
+
+model.fit(
+    X, y, epochs=epochs, callbacks=callbacks, validation_split=0.2,
+)
