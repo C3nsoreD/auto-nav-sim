a3331d4de25cc09466b73299b7d75e474953a246
Gabriel C3n <ur.gabriel@outlook.com>
2020-06-09 11:20:37 +0200

    Test model

--- image-processing/cnn-new/cnn-model.py
+++ image-processing/cnn-new/cnn-model.py
@@ -1,10 +1,12 @@
+import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
 from matplotlib import pyplot as plt
 import pickle
-import keras
-
 import numpy as np
+import cv2
+import numpy as np
+
 # features
 X = pickle.load(open("X.pickle", "rb"))
 # labels
@@ -51,7 +53,14 @@ def main(_size, epochs):
 
     history = model.fit(X, y, batch_size=_size, epochs=epochs, validation_split=0.2)
     model.save_weights('my_model.h5')
+    # pmodel = model.load_weights('my_model.h5')
     # list all data in history
+    # test_x = np.array(X[20])
+    # print(test_x.shape)
+    # test_x_reshape = test_x.reshape(70, 70, 1)
+    # predictions = model.predict(test_x_reshape)
+    # print(predictions)
+
     print(history.history.keys())
     # summarize history for accuracy
     plt.plot(history.history['accuracy'])
@@ -71,4 +80,24 @@ def main(_size, epochs):
     plt.show()
 
 
-main(16, 50)
+def test():
+    img = tf.keras.preprocessing.image.load_img(
+        "images/safe/3.jpg", target_size=(70, 70)
+    )
+    img_array_cv = cv2.imread("images/safe/3.jpg", cv2.IMREAD_GRAYSCALE)
+    # img_array = tf.keras.preprocessing.image.img_to_array(img)
+    # img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+    img_array = np.expand_dims(img_array_cv, axis=0)
+    model = main(16, 20)
+    # model = model.load_weights('my_model.h5')
+
+    predictions = model.predict(img_array)
+    score = predictions[0]
+    print(
+        "This image is %.2f percent cat and %.2f percent dog."
+        % (100 * (1 - score), 100 * score)
+    )
+
+
+main(16, 20)
+# test()
